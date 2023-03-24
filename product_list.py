69348167198d558053586d13377298179fa88db5
CHI Jason <jason921008@gmail.com>
2023-03-24 11:43:32 +0900

    second commit

--- product_list.py
+++ product_list.py
@@ -1,6 +1,18 @@
+import os
 products = []
+if os.path.isfile('products.csv'):
+    print("File exist")
+    with open('products.csv', 'r', encoding = 'utf8' ) as f:
+        for line in f:
+            if 'products,price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+            print(products)
+else:
+    print("File doesn't exist")
 
-
+# Let users enter        
 while True:
     name = input('enter products name:')
     if name == "q":
@@ -9,10 +21,10 @@ while True:
     p = [name, price]
     products.append(p)
 print(products)
-
+# Print purchase history
 for d in products:
     print(d[0], d[1])
-
+# write a file
 with open('products.csv', 'w', encoding = 'utf8') as f:
     f.write('products,price' + '\n')
     for d in products:
